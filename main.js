49bfc4629171be0230e2fc235a28ca68ab821733
Jakub Horcicka <jakub.horcicka@biesdorf.cz>
2018-03-17 11:06:04 +0100

    Initial upload of the project, first files.

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * @param string text
+ */
+function debug(text) {
+  console.log("DEBUG: " + text);
+}
+
+/**
+ * @param number x
+ * @param number y
+ * @return number
+ */
+function add(x, y) {
+  return x + y;
+}
+
