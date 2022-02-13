ccfcf508f77beb5a7a14ab2214fb0fc57cf76933
dev@meico.cz <dev@meico.cz>
2022-02-13 18:48:45 +0100

    Angular reactive forms.

--- angular2-forms/src/app/app.module.ts
+++ angular2-forms/src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TemplateFormComponent } from './template-form/template-form.component'
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component'
 
 @NgModule({
   declarations: [
@@ -16,6 +15,7 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- angular2-forms/src/app/reactive-form/reactive-form.component.ts
+++ angular2-forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+import { Country } from '../model/country'
 
 @Component({
   selector: '',
@@ -6,4 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive-form.component.sass']
 })
 export class ReactiveFormComponent {
+  countryList: Country[]
+  contactForm: FormGroup
+
+  constructor() {
+    this.countryList = [
+      new Country("1", "CZ"),
+      new Country("2", "NL"),
+      new Country("3", "TH")
+    ]
+    this.contactForm = new FormGroup({
+      firstname: new FormControl('default-first-name', [Validators.required,Validators.minLength(5)]),
+      lastname: new FormControl({ value: 'default-last-name', disabled: true }),
+      email: new FormControl('@', [Validators.email,Validators.required]),
+      gender: new FormControl(),
+      isMarried: new FormControl(),
+      country: new FormControl(),
+      address:new FormGroup({
+        city: new FormControl(),
+        street: new FormControl(),
+        pincode:new FormControl()
+      })
+    })
+  }
+
+  getCountryList(): Country[] {
+    return this.countryList
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value)
+  }
 }
